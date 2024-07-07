6acdf8705dc00a1dd521974fbf6cdfede34b9980
Tilek <timur.almamatov@yandex.ru>
2024-07-08 03:42:25 +0700

    added blocke settimeout

--- tg motivation bot/functions/checkDeadlineAndNotify.js
+++ tg motivation bot/functions/checkDeadlineAndNotify.js
@@ -1,6 +1,8 @@
-const {getNewQuote}  = require('./getNewQuote');
-const {checkEnd}  = require('./checkEnd');
-function checkDeadlineAndNotify(bot, chatId,deadlineString,idProgress) {
+const {getNewQuote} = require('./getNewQuote');
+const {checkEnd} = require('./checkEnd');
+const {downloadRes} = require('./downloadRes');
+
+function checkDeadlineAndNotify(bot, chatId, deadlineString, idProgress) {
     // Получаем сегодняшнюю дату
     const today = new Date();
 
@@ -11,51 +13,62 @@ function checkDeadlineAndNotify(bot, chatId,deadlineString,idProgress) {
     const deadline = new Date(deadlineString);
 
     // Вычисляем разницу во времени в миллисекундах
-    // const timeDifferenceInMilliseconds = deadline.getTime() - today.getTime();
-    const timeDifferenceInMilliseconds = 1000*60*2;
-    let monthInMilliseconds = 1000*60*60*24*30;
-    let firstWeek = 1000*60*60*7;//неделя
-    let firstFithDays = 1000*60*60*15;//15 число
-    let weekBeforeFinished = timeDifferenceInMilliseconds-firstWeek;//23 число
+    const timeDifferenceInMilliseconds = 1000 * 60 * 2;
+    const monthInMilliseconds = 1000 * 60 * 60 * 24 * 30;
+    const firstWeek = 1000 * 60 * 60 * 7; // неделя
+    const firstFifteenDays = 1000 * 60 * 60 * 15; // 15 число
+    const weekBeforeFinished = timeDifferenceInMilliseconds - firstWeek; // 23 число
+
+    // Массив для хранения идентификаторов таймеров
+    let timeouts = [];
+
     // Если время уже прошло, выводим сообщение
     if (timeDifferenceInMilliseconds <= 0) {
         bot.sendMessage(chatId, 'Все сроки наступили');
         return;
-
     } else if (timeDifferenceInMilliseconds >= monthInMilliseconds) {
-        setTimeout(() => {
+        timeouts.push(setTimeout(() => {
             getNewQuote(bot, chatId);
-
-        }, firstWeek);
-        setTimeout(() => {
+        }, firstWeek));
+        timeouts.push(setTimeout(() => {
             getNewQuote(bot, chatId, 'yes');
-
-        }, firstFithDays);
-        setTimeout(() => {
+        }, firstFifteenDays));
+        timeouts.push(setTimeout(() => {
             getNewQuote(bot, chatId, 'yes');
-
-        }, weekBeforeFinished);
-    } else if(timeDifferenceInMilliseconds<monthInMilliseconds) {
-        let half = timeDifferenceInMilliseconds/2;
-        setTimeout(() => {
+        }, weekBeforeFinished));
+    } else if (timeDifferenceInMilliseconds < monthInMilliseconds) {
+        const half = timeDifferenceInMilliseconds / 2;
+        timeouts.push(setTimeout(() => {
             getNewQuote(bot, chatId, 'yes');
-
-        }, half);
+        }, half));
     }
 
     // Устанавливаем таймер, который будет активироваться через разницу во времени
-    setTimeout(() => {
+    timeouts.push(setTimeout(() => {
         bot.sendMessage(chatId, 'Все сроки наступили');
-        checkEnd(bot, chatId, idProgress)
-    }, timeDifferenceInMilliseconds);
+        checkEnd(bot, chatId, idProgress);
+    }, timeDifferenceInMilliseconds));
 
-    //сообщение на ранее фото результат
+    // Реакция на отправку файла (фото)
     bot.on('photo', async img => {
         try {
+            // Очистка всех таймеров
+            timeouts.forEach(timeout => clearTimeout(timeout));
+
             await bot.sendMessage(chatId, "Вау! Я вижу ты реализовал свою цель раньше! \nДавай зафиксируем результат.💪\nНапиши команду /result");
         } catch (error) {
             console.error('Ошибка при отправке сообщения:', error);
         }
     });
+
+    // Обработка команды /result
+    bot.onText(/\/result/, async msg => {
+        try {
+            await downloadRes(bot, chatId);
+        } catch (error) {
+            console.error('Ошибка при вызове downloadRes:', error);
+        }
+    });
 }
+
 module.exports = {checkDeadlineAndNotify};
---
+++ tg-bot-frontend/src/components/last-quiz/lastQuiz.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import './quizPage.scss';
+
+const lastQuiz = () => {
+    const [formData, setFormData] = useState({});
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    // Функция для получения параметров из URL
+    const getParamsFromUrl = () => {
+        const params = new URLSearchParams(window.location.search);
+        const userId = params.get('userId');
+        const goalId = params.get('goalId');
+        return { userId, goalId };
+    }
+
+    const handleInputChange1 = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleInputChange2 = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleInputChange3 = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleInputChange4 = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleInputChange5 = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value });
+        console.log(formData)
+    };
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (Object.keys(formData).length === 0) {
+            alert("Пожалуйста, заполните все поля.");
+            return;
+        }
+        setIsSubmitted(true);
+        try {
+            let formattedData = Object.entries(formData).map(([key, value]) => `${key}:${value}`).join(',');
+            // Получаем параметры из URL
+            const { userId, goalId } = getParamsFromUrl();
+            const response = await fetch(`http://localhost:3000/quiz?userId=${userId}&goalId=${goalId}`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    goalId: +goalId,
+                    answers: formattedData,
+                    userId: +userId,
+                    dateFilled: new Date().toISOString()
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+            if (response.ok) {
+                setShowSuccessMessage(true);
+            } else {
+                throw new Error('Ошибка при отправке данных.');
+            }
+        } catch (error) {
+            console.error('Ошибка:', error);
+        }
+    };
+
+    return (
+        <div className="content">
+            {!isSubmitted? ( // Проверяем, была ли форма отправлена
+                <div className="form">
+                    <h1>Опросник мотивации на достижение цели⚡️</h1>
+                    <form onSubmit={handleSubmit}>
+                    <p><label htmlFor="question1">Вопрос 1: Что будет, когда ты достигнешь цели?</label></p>
+                    <input type="text" id="question1" name="question1" onChange={handleInputChange1} required /><br /><br />
+
+                    <p><label htmlFor="question2">Вопрос 2: Насколько ты далек от цели в данный момент?</label></p>
+                    <input type="text" id="question2" name="question2" onChange={handleInputChange2} required /><br /><br />
+                    <p><label htmlFor="question3">Вопрос 3: Насколько сильное твое желаение достичь цели?</label></p>
+                    <input type="text" id="question3" name="question3" onChange={handleInputChange3} required /><br /><br />
+
+                    <p><label htmlFor="question4">Вопрос 4: Что сделаешь после того как достигнешь?</label></p>
+                    <input type="text" id="question4" name="question4" onChange={handleInputChange4} required /><br /><br />
+
+                    <p><label htmlFor="question5">Вопрос 5: Веришь в себя?</label></p>
+                    <input type="text" id="question5" name="question5" onChange={handleInputChange5} required /><br /><br />
+                    <button type="submit">Отправить</button>
+                    </form>
+                </div>
+            ) : (
+                showSuccessMessage && (
+                    <div className="go-to">
+                        <h1>Подздравляю Ты прошел опрос!</h1>
+                        <a href="https://t.me/ProDVIGok_bot">вернутся к боту</a>
+                    </div>
+                )
+            )}
+        </div>
+    );
+};
+
+export default lastQuiz;
